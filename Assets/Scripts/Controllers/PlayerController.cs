20627ce7dfedad14ed444e7cc30fecd183d55874
Mathijs Lemmers <mathijs750@gmail.com>
2016-12-11 10:35:16 +0100

    Add pathfinding to project

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float searchStopDistance = 1f;
+    [SerializeField]
+    private GameObject targetPoint;
     private bool hasWrench = false;
     private RaycastHit2D hit;
     private Vector3 newPosition;
+    private AILerp lerp;
 
 	void Awake()
     {
-		
+        lerp = GetComponent<AILerp>();
 	}
 	
 
@@ -22,16 +27,20 @@ public class PlayerController : MonoBehaviour
             {
                 hit = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 
                     Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero,0f);
-                if (hit.transform.tag == "Interactable") { hit.transform.GetComponent<IInteractable>().Interact(); }
+                if (hit)
+                {
+                    if (hit.transform.tag == "Interactable") { hit.transform.GetComponent<IInteractable>().Interact(); }
+
+                }
             }
 
-            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime *3);
-            if (Vector3.Distance(newPosition, transform.position) < 0.5f) { transform.position = newPosition; }
+            if (Vector3.Distance(transform.position, targetPoint.transform.position) < searchStopDistance) { lerp.canSearch = false; }
         }
 	}
 
     public void setMovePoint(Vector3 position, int turnAmount)
     {
-        newPosition = position;
+        targetPoint.transform.position = position;
+        lerp.canSearch = true;
     }
 }
--- Assets/Scripts/Interactables/WalkArea.cs
+++ Assets/Scripts/Interactables/WalkArea.cs
@@ -1,16 +1,22 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class WalkArea : MonoBehaviour, IInteractable
 {
+    [SerializeField]
+    private PlayerController player;
+    [SerializeField]
+    private int turn = 0;
+
     void IInteractable.Interact()
     {
-
+        player.setMovePoint(Camera.main.ScreenToWorldPoint( Input.mousePosition)+ new Vector3(0,0,10), turn);
     }
 
-    public float waitSeconds = 0;
-    public float speedOut = 0;
+    void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.color = new Color(0, 0, 1, 0.6f);
+        Gizmos.DrawCube(Vector3.zero, GetComponent<BoxCollider2D>().size);
+    }
 }
