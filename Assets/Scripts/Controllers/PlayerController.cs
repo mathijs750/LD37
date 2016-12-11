9d88ac510a5d74e55ec3d8bf674b4f6113773fd4
Mathijs Lemmers <mathijs750@gmail.com>
2016-12-11 01:41:11 +0100

    Add waypoint path thing

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private bool hasWrench = false;
     private RaycastHit2D hit;
+    private Vector3 newPosition;
 
 	void Awake()
     {
@@ -23,6 +24,14 @@ public class PlayerController : MonoBehaviour
                     Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero,0f);
                 if (hit.transform.tag == "Interactable") { hit.transform.GetComponent<IInteractable>().Interact(); }
             }
+
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime *3);
+            if (Vector3.Distance(newPosition, transform.position) < 0.5f) { transform.position = newPosition; }
         }
 	}
+
+    public void setMovePoint(Vector3 position, int turnAmount)
+    {
+        newPosition = position;
+    }
 }
---
+++ Assets/Scripts/Interactables/WalkArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WalkArea : MonoBehaviour, IInteractable
+{
+    void IInteractable.Interact()
+    {
+
+    }
+
+    public float waitSeconds = 0;
+    public float speedOut = 0;
+}
