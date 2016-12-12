fcb3e445f83f0691dba2328aa225b28feb50b7ea
Mathijs Lemmers <mathijs750@gmail.com>
2016-12-12 23:50:40 +0100

    Add character movement

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -10,26 +10,32 @@ public class PlayerController : MonoBehaviour
     private GameObject targetPoint;
     private bool hasWrench = false;
     private RaycastHit2D hit;
-    private Vector3 newPosition;
+    private Vector3 newPosition, lastPosition;
     private AILerp lerp;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     private GameObject _holdObject;
     public GameObject holdObject { get { return _holdObject; } }
 
 
-	void Awake()
+    void Awake()
     {
         lerp = GetComponent<AILerp>();
-	}
-	
+        animator = transform.GetChild(0).GetComponent<Animator>();
+        spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        lastPosition = transform.position;
+    }
 
-	void Update ()
+
+    void Update()
     {
-		if (GameManager.instance.globalState == GlobalState.Gameplay)
+        if (GameManager.instance.globalState == GlobalState.Gameplay)
         {
             if (Input.GetMouseButtonUp(0))
             {
-                hit = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 
-                    Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero,0f);
+                hit = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                    Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero, 0f);
                 if (hit)
                 {
                     if (hit.transform.tag == "Interactable") { hit.transform.GetComponent<IInteractable>().Interact(); }
@@ -38,8 +44,23 @@ public class PlayerController : MonoBehaviour
             }
 
             if (Vector3.Distance(transform.position, targetPoint.transform.position) < searchStopDistance) { lerp.canSearch = false; }
+
+
+            hit = Physics2D.Raycast(transform.position, Vector2.zero, 1f);
+            if (hit)
+            {
+                if (hit.transform.name == "Walk") { animator.SetBool("isHanging", false); }
+                else { animator.SetBool("isHanging", true); }
+            }
+            Vector3 diff = transform.position - targetPoint.transform.position;
+
+            Debug.Log(Mathf.Abs(diff.x * 10f));
+            animator.SetFloat("Horizontalspeed", diff.x);
+            animator.SetFloat("Verticalspeed", diff.y);
+            if (diff.x < 0) { spriteRenderer.flipX = true; }
+            else { spriteRenderer.flipX = false; }
         }
-	}
+    }
 
     public void setMovePoint(Vector3 position, int turnAmount)
     {
